7a01fc50e4901bd24b823aff3088212232a2eefb
Igor <tkachenko.ic@gmail.com>
2020-09-05 17:30:20 +0200

    Added all necessary dependencies
    Particularly Hilt

---
+++ app/src/main/java/com/quizsquiz/bookcollectionapp/module/ApplicationModule.kt
@@ -0,0 +1,12 @@
+package com.quizsquiz.bookcollectionapp.module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class ApplicationModule {
+
+}
